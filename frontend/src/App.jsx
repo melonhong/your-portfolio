cb26d128ffa3ad742d04615ac45ea7ce6d2e78ab
melonhong <melonhong46@gmail.com>
2025-03-13 23:58:01 +0900

    feat: backend에서 메시지 수령

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [msg, setMsg] = useState("");
 
-  return (
-    <>
-      <h1>Hello World!</h1>
-    </>
-  );
+  useEffect(() => {
+    async function getMsg() {
+      try {
+        const json = await (await fetch("http://localhost:8080/")).json(); // 서버에 API를 통해 JSON 데이터 요청
+        setMsg(json.message);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getMsg();
+  }, []);
+
+  return <>{loading ? <h1>Loading...</h1> : <h1>dfsfsd {msg}</h1>}</>;
 }
 
 export default App;
