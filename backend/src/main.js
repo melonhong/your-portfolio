a8c1d12809eb918f66c74a362186f44eec34d141
melonhong <melonhong46@gmail.com>
2025-03-13 23:57:47 +0900

    feat: corsMiddleware 사용

--- backend/src/main.js
+++ backend/src/main.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+const corsMiddleware = require("./middlewares/cors");
+
 // 포트 설정
 app.set("port", process.env.PORT || 8080);
 
 // 로그 설정
 app.use(morgan("combined"));
+
+// CORS 에러 해결
+app.use(corsMiddleware);
 
 // 라우팅
 app.get("/", (req, res) => {
