fbd6c77b68dd4de7168feefcc728dbc5aeb0fdad
melonhong <melonhong46@gmail.com>
2025-03-14 23:55:56 +0900

    feat: 허용할 출처 추가

--- backend/src/middlewares/cors.js
+++ backend/src/middlewares/cors.js
@@ -1,6 +1,6 @@
 const cors = require("cors");
 
-const allowedOrigins = ["http://localhost:5173"]; // 허용할 출처 목록
+const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5173"]; // 허용할 출처 목록
 
 const corsOptions = {
   origin: function (origin, callback) {
