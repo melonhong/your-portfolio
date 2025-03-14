b521637b37324d9830a5e7370cdbccff831aa702
melonhong <melonhong46@gmail.com>
2025-03-14 16:44:32 +0900

    feat: Home 컴포넌트 불러오기

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,12 @@
 import React, { useState, useEffect } from "react";
+import Home from "./components/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [msg, setMsg] = useState("");
-
-  useEffect(() => {
-    async function getMsg() {
-      try {
-        const json = await (await fetch("http://localhost:8080/")).json(); // 서버에 API를 통해 JSON 데이터 요청
-        setMsg(json.message);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setLoading(false);
-      }
-    }
-    getMsg();
-  }, []);
-
-  return <>{loading ? <h1>Loading...</h1> : <h1>{msg}</h1>}</>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
