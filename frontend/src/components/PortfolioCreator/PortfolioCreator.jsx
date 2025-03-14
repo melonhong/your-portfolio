e94f3c47a7844f19970a1757807c1165f69720f4
melonhong <melonhong46@gmail.com>
2025-03-15 00:13:47 +0900

    docs: PortfolioCreator 컴포넌트 생성

---
+++ frontend/src/components/PortfolioCreator/PortfolioCreator.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+
+const PortfolioCreator = () => {
+  return (
+    <>
+      <h1>Create your portfolio!</h1>
+    </>
+  );
+};
+export default PortfolioCreator;
