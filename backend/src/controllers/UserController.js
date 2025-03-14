07d438c769fc74ab54fdd09340370c6ec5d2bda6
melonhong <melonhong46@gmail.com>
2025-03-14 18:25:13 +0900

    docs: UserController 추가

---
+++ backend/src/controllers/UserController.js
@@ -0,0 +1,30 @@
+const UserService = require("../services/UserService");
+const { User } = require("../models");
+
+class UserController {
+  constructor() {
+    this.userService = new UserService(User); // UserService 인스턴스 생성 (모델을 전달)
+  }
+
+  // 사용자 생성 또는 조회
+  async findOrCreateUser(req, res) {
+    try {
+      const { email, username } = req.user;
+      const originalUsername = username.familyName + username.givenName;
+      console.log(originalUsername);
+      const [user, created] = await this.userService.findOrCreate({
+        originalUsername,
+        email,
+      });
+
+      req.session.userId = user.id; // 사용자 ID를 세션에 저장
+      res.redirect("http://localhost:5173"); // 프론트엔드로 리디렉션
+    } catch (error) {
+      console.error("Error in findOrCreateUser:", error); // 에러 출력
+      res.status(400).json({ message: error.message }); // 에러 처리
+    }
+  }
+}
+
+// UserController 클래스를 모듈로 내보냄
+module.exports = UserController;
