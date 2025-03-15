7ec669456605436d7dcf6d0f26c976ed62b1a5ba
melonhong <melonhong46@gmail.com>
2025-03-15 21:39:11 +0900

    feat: fetchPortfolio 메서드 추가

--- frontend/src/components/PortfolioCreator/PortfolioForm.jsx
+++ frontend/src/components/PortfolioCreator/PortfolioForm.jsx
@@ -1,13 +1,39 @@
 import React, { useEffect, useState } from "react";
 
 const PortfolioForm = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const fetchPortfolio = async (event) => {
+    event.preventDefault();
+    try {
+      const portfolioData = { title, description };
+      const data = await (
+        await fetch("http://localhost:8080/portfolio/create", {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(portfolioData),
+        })
+      ).json();
+      console.log(data);
+      // 생성한 포트폴리오 디테일로 리다이렉션
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={fetchPortfolio}>
       <div className="mb-3">
         <label htmlFor="portfolio-title" className="form-label">
           Enter Portfolio Title
         </label>
         <input
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
           type="text"
           className="form-control"
           id="portfolio-title"
@@ -18,6 +44,8 @@ const PortfolioForm = () => {
         <div className="input-group">
           <span className="input-group-text">Description</span>
           <textarea
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
             className="form-control"
             aria-label="Descript your portfolio"
           ></textarea>
