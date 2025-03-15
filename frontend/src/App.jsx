0535041d65321b6cedc9b1ff4349b348a6b09713
melonhong <melonhong46@gmail.com>
2025-03-15 23:27:22 +0900

    feat: /login 라우트 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import PortfolioDetail from "./components/PortfolioDetail/PortfolioDetail";
 import PortfolioCreator from "./components/PortfolioCreator/PortfolioCreator";
+import Login from "./components/Auth/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/portfolio/create" element={<PortfolioCreator />} />
         <Route path="/portfolio/:id" element={<PortfolioDetail />} />
       </Routes>
