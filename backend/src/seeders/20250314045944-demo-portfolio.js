075e4e3bf95f6a120688727113428e11d50cff73
melonhong <melonhong46@gmail.com>
2025-03-15 21:40:07 +0900

    feat: 중복 확인 코드 추가

--- backend/src/seeders/20250314045944-demo-portfolio.js
+++ backend/src/seeders/20250314045944-demo-portfolio.js
@@ -2,15 +2,25 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert("portfolios", [
+    const exist = await queryInterface.rawSelect(
+      "portfolios",
       {
-        user_id: 1, // 실제로 존재하는 user_id를 설정해야 함
-        title: "Test Portfolio",
-        description: "This is a test portfolio description.",
-        createdAt: new Date(),
-        updatedAt: new Date(),
+        where: { user_id: "1" },
       },
-    ]);
+      ["id"] // 특정 컬럼 선택 (존재 여부만 확인)
+    );
+
+    if (!exist) {
+      await queryInterface.bulkInsert("portfolios", [
+        {
+          user_id: 1, // 실제로 존재하는 user_id를 설정해야 함
+          title: "Test Portfolio",
+          description: "This is a test portfolio description.",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ]);
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
