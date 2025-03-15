2d8e59bef6d79029022e90f28f9f2c4ee6f54a39
melonhong <melonhong46@gmail.com>
2025-03-15 21:40:25 +0900

    feat: findByPortfolio 메서드 추가

--- backend/src/services/PortfolioService.js
+++ backend/src/services/PortfolioService.js
@@ -15,23 +15,40 @@ class PortfolioService {
     }
   }
 
+  // 포트폴리오 아이디로 포트폴리오 조회
+  async findByPortfolioId(portfolioId) {
+    try {
+      const portfolio = await this.portfolioModel.findByPk(portfolioId); // portfolioId를 인자로 전달
+      return portfolio;
+    } catch (error) {
+      throw new Error("Failed to find portfolio: " + error.message);
+    }
+  }
+
   // 유저 아이디로 포트폴리오 생성
   async createByUserId(userId, portfolioData) {
     try {
-      const prePortfolio = await this.findByUserId(userId);
-      const newPortfolio = null;
-      if (prePortfolio === null) {
-        // 이전에 생성한 포트폴리오가 없다면 새로운 포트폴리오 생성
-        newPortfolio = await this.portfolioModel.create({
+      // 사용자 ID로 기존 포트폴리오 찾기
+      const existingPortfolio = await this.findByUserId(userId);
+
+      // 기존 포트폴리오가 없는 경우에만 새 포트폴리오 생성
+      if (!existingPortfolio) {
+        const newPortfolio = await this.portfolioModel.create({
           user_id: userId,
           title: portfolioData.title,
           description: portfolioData.description,
         });
-      }
 
-      return newPortfolio;
+        return newPortfolio; // 새 포트폴리오 반환
+      } else {
+        // 이미 존재하는 포트폴리오가 있을 때의 처리
+        throw new Error("Portfolio already exists for this user.");
+      }
     } catch (error) {
-      throw new Error("Failed to create portfolio: " + error);
+      // 에러 발생 시 유용한 정보 포함
+      throw new Error(
+        `Failed to create portfolio for user ID ${userId}: ${error.message}`
+      );
     }
   }
 }
