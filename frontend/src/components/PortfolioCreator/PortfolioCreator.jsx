417392da016a502c88200315e84eba66f2ae1629
melonhong <melonhong46@gmail.com>
2025-03-15 23:28:31 +0900

    feat: checkAuth 커스텀 훅 사용

--- frontend/src/components/PortfolioCreator/PortfolioCreator.jsx
+++ frontend/src/components/PortfolioCreator/PortfolioCreator.jsx
@@ -1,11 +1,23 @@
 import React, { useEffect, useState } from "react";
 import PortfolioForm from "./PortfolioForm";
+import { checkAuth } from "../../utils/checkAuth";
+import Login from "../Auth/Login";
 
 const PortfolioCreator = () => {
+  const { user, loading } = checkAuth();
+
   return (
     <>
-      <h1>Create your portfolio!</h1>
-      <PortfolioForm />
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : user ? (
+        <>
+          <h1>Create your portfolio!</h1>
+          <PortfolioForm />
+        </>
+      ) : (
+        <Login />
+      )}
     </>
   );
 };
