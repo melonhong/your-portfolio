7140f9b77141de12f4da008640fa52f748fcc1d6
melonhong <melonhong46@gmail.com>
2025-03-16 11:48:11 +0900

    chore: /portfolio/main 라우트 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import PortfolioDetail from "./components/PortfolioDetail/PortfolioDetail";
 import PortfolioCreator from "./components/PortfolioCreator/PortfolioCreator";
+import PortfolioMain from "./components/PortfolioMain/PortfolioMain";
 import Login from "./components/Auth/Login";
 import Redirect from "./components/Common/Redirect";
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/portfolio/create" element={<PortfolioCreator />} />
+        <Route path="/portfolio/main" element={<PortfolioMain />} />
         <Route path="/portfolio/:id" element={<PortfolioDetail />} />
         <Route path="/redirect" element={<Redirect />} />
       </Routes>
