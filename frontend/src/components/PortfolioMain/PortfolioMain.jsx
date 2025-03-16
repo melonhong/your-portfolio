75d137f03a767b07a9605b530a06b4fef0ceecc7
melonhong <melonhong46@gmail.com>
2025-03-16 22:50:54 +0900

    feat: fetchPortfolios 메서드 추가

--- frontend/src/components/PortfolioMain/PortfolioMain.jsx
+++ frontend/src/components/PortfolioMain/PortfolioMain.jsx
@@ -1,15 +1,36 @@
+import { useEffect, useState } from "react";
+import PortfolioCard from "./PortfolioCard";
+
 const PortfolioMain = () => {
+  const [portfolios, setPortfolios] = useState([]);
   // 페이징 단위로 포트폴리오를 가져오는 fetch 함수 추가
+  useEffect(() => {
+    const fetchPortfolios = async () => {
+      const data = await (
+        await fetch(`http://localhost:8080/portfolio/list`)
+      ).json();
+      console.log(data);
+      setPortfolios(data);
+    };
+    fetchPortfolios();
+  }, []);
   return (
     <>
       <h1>View all portfolios!</h1>
       <div className="container">
         <div className="row">
           {/* map으로 PortfolioCard 추가 */}
-          <div className="col">col</div>
-          <div className="col">col</div>
-          <div className="col">col</div>
-          <div className="col">col</div>
+          {portfolios.map((portfolio) => {
+            return (
+              <div className="col" key={portfolio.id}>
+                <PortfolioCard
+                  id={portfolio.id}
+                  title={portfolio.title}
+                  description={portfolio.description}
+                />
+              </div>
+            );
+          })}
         </div>
       </div>
 
