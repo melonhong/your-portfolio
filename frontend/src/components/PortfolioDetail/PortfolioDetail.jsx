b408e91ca19b2f611af35d2305c2dff89eefa426
melonhong <melonhong46@gmail.com>
2025-03-18 00:14:38 +0900

    feat: 편집 버튼 추가
    - 자신의 userId와 포트폴리오의 user_id가 일치할 때만 보임

--- frontend/src/components/PortfolioDetail/PortfolioDetail.jsx
+++ frontend/src/components/PortfolioDetail/PortfolioDetail.jsx
@@ -1,22 +1,41 @@
 import { getPortfolio } from "../../utils/getPortfolio";
+import { checkAuth } from "../../utils/checkAuth";
 
 const PortfolioDetail = () => {
+  const { user, loading } = checkAuth();
   const portfolio = getPortfolio();
+
   return (
     <>
-      <h1>Portfolio Detail</h1>
-      <div>
-        {portfolio ? (
-          <>
-            <h2>{portfolio.title}</h2>
-            <p>{portfolio.description}</p>
-          </>
-        ) : (
-          <>
-            <h2>No Portfolio</h2>
-          </>
-        )}
-      </div>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <>
+          <h1>Portfolio Detail</h1>
+          <div>
+            {portfolio ? (
+              <>
+                <h2>{portfolio.title}</h2>
+                <p>{portfolio.description}</p>
+                <div>
+                  {portfolio.user_id === user.userId ? (
+                    <a
+                      href={`/portfolio/edit/${portfolio.id}`}
+                      className="btn btn-primary"
+                    >
+                      Edit
+                    </a>
+                  ) : null}
+                </div>
+              </>
+            ) : (
+              <>
+                <h2>No Portfolio</h2>
+              </>
+            )}
+          </div>
+        </>
+      )}
     </>
   );
 };
