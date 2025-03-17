10e49923c94ecdf26ce52c5ea837efceb796cc19
melonhong <melonhong46@gmail.com>
2025-03-17 23:04:10 +0900

    feat: PortfolioForm 불러오기

--- frontend/src/components/PortfolioEdit/PortfolioEdit.jsx
+++ frontend/src/components/PortfolioEdit/PortfolioEdit.jsx
@@ -1,25 +1,11 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import { getPortfolio } from "../../utils/getPortfolio";
 
 const PortfolioEdit = () => {
-  const portfolio = getPortfolio();
-
+  const prePortfolio = getPortfolio();
   return (
     <>
-      <h1>Portfolio Edit</h1>
-      <div>
-        {portfolio ? (
-          <>
-            <h2>{portfolio.title}</h2>
-            <p>{portfolio.description}</p>
-          </>
-        ) : (
-          <>
-            <h2>No Portfolio</h2>
-          </>
-        )}
-      </div>
+      <h1>Create your portfolio!</h1>
+      <PortfolioForm method="PATCH" prePortfolio={prePortfolio} />
     </>
   );
 };
